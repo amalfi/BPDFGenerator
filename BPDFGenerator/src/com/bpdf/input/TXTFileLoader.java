01d65ffee6383b99bd74c71b6d10139a73b57223
amalfi <mberendt546@gmail.com>
2014-12-04 21:04:49 +0100

    [desc]First Commit

---
+++ BPDFGenerator/src/com/bpdf/input/TXTFileLoader.java
@@ -0,0 +1,22 @@
+package com.bpdf.input;
+
+/**
+ * Class which contains method for loading TXT file from disc
+ * @author Marcin Berendt
+ *
+ */
+public class TXTFileLoader 
+{
+	/**
+	 * 
+	 * @param sTXTPath - disc path to file
+	 * @return TXTFile content as a String object
+	 */
+	public String loadTXTFileFromDisc(String sTXTPath)
+	{
+		String sTXTFile="";
+		
+		return sTXTFile;
+	}
+	
+}
---
+++ BPDFGenerator/src/com/bpdf/main/Start.java
@@ -0,0 +1,15 @@
+package com.bpdf.main;
+/**
+ * Main class 
+ * @author Marcin Berendt
+ *
+ */
+public class Start {
+
+	public static void main(String[] args) 
+	{
+
+
+	}
+
+}
