a7ed5bd73caba4f2920d2199845560880e30dc2b
amalfi <mberendt546@gmail.com>
2014-12-06 11:45:37 +0100

    [desc]Generating pdf with content from txt file

--- BPDFGenerator/src/com/bpdf/input/TXTFileLoader.java
+++ BPDFGenerator/src/com/bpdf/input/TXTFileLoader.java
@@ -1,5 +1,10 @@
 package com.bpdf.input;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Class which contains method for loading TXT file from disc
  * @author Marcin Berendt
@@ -11,12 +16,31 @@ public class TXTFileLoader
 	 * 
 	 * @param sTXTPath - disc path to file
 	 * @return TXTFile content as a String object
+	 * @throws IOException 
 	 */
-	public String loadTXTFileFromDisc(String sTXTPath)
+	public String loadTXTFileFromDisc(String sTXTPath) throws IOException
 	{
 		String sTXTFile="";
-		
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(sTXTPath)));
+		try 
+		{
+		    String line;
+		    while ((line = br.readLine()) != null)
+		    {
+		    	stringBuilder.append(line);
+		    }
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		finally 
+		{
+		    br.close();
+		}
+		sTXTFile=stringBuilder.toString();
 		return sTXTFile;
+
 	}
-	
 }
--- BPDFGenerator/src/com/bpdf/main/Start.java
+++ BPDFGenerator/src/com/bpdf/main/Start.java
@@ -1,4 +1,12 @@
 package com.bpdf.main;
+
+import java.io.IOException;
+
+import org.apache.pdfbox.exceptions.COSVisitorException;
+
+import com.bpdf.input.TXTFileLoader;
+import com.bpdf.pdf.BPDFDocumentGenerator;
+
 /**
  * Main class 
  * @author Marcin Berendt
@@ -8,7 +16,18 @@ public class Start {
 
 	public static void main(String[] args) 
 	{
-
+		TXTFileLoader txtFileLoader = new TXTFileLoader();
+		try 
+		{
+			String sFileContent = txtFileLoader.loadTXTFileFromDisc("D:\\text.txt");
+			System.out.println(sFileContent);
+			BPDFDocumentGenerator bpdfdocumentGenerator = new BPDFDocumentGenerator();
+			bpdfdocumentGenerator.generatePDFDocumentWithContentAsParameter("D:\\text.pdf", sFileContent);
+		}
+		catch (IOException | COSVisitorException e) 
+		{
+			e.printStackTrace();
+		}
 
 	}
 
